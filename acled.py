112c863071f700139221033118da583cebcf111c
dgootman <gootman@gmail.com>
2024-03-12 02:04:21 +0000

    Download and render selected datasets

--- acled.py
+++ acled.py
@@ -1,11 +1,25 @@
+import csv
+import shutil
+from datetime import date
+from email.message import Message
+from pathlib import Path
+from typing import Callable
+
+import pandas as pd
+import plotly.express as px
 import requests
 import streamlit as st
 from bs4 import BeautifulSoup
 from streamlit.logger import get_logger
+from xlsx2csv import Xlsx2csv
 
 LOGGER = get_logger(__name__)
 
 url = "https://acleddata.com/curated-data-files"
+
+tmp_path = Path("/tmp")
+data_path = Path("data")
+data_path.mkdir(exist_ok=True)
 
 
 @st.cache_resource(ttl="10 min")
@@ -49,22 +63,15 @@ def start_session() -> requests.Session:
     response.raise_for_status()
     return session
 
-def main():
-    st.set_page_config(
-        page_title="ACLED",
-        page_icon="⚔️",
-    )
 
-    st.title("ACLED")
-
-    session = start_session()
-
-    response = session.get(url)
+@st.cache_data(ttl="1 day")
+def load_datasets() -> list[dict]:
+    response = start_session().get(url)
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, "html.parser")
 
-    datasets = [
+    return [
         {
             "name": download_box_content.find("h1").get_text(strip=True),
             "url": download_box_content.find("a")["href"],
@@ -74,8 +81,133 @@ def main():
         )
     ]
 
-    st.header("Datasets")
-    st.dataframe(datasets)
+
+def download_dataset(dataset):
+    name = dataset["name"]
+
+    r = start_session().get(dataset["url"], stream=True)
+    r.raise_for_status()
+
+    m = Message()
+    for key, value in r.headers.items():
+        m[key] = value
+    filename = m.get_param(
+        "filename", failobj=[f"{name}.xlsx"], header="content-disposition"
+    )[-1]
+
+    if not (data_path / filename).exists():
+        temp_file = tmp_path / filename
+        with temp_file.open("wb") as f:
+            for chunk in r.iter_content(1024):
+                f.write(chunk)
+        shutil.move(temp_file, data_path / filename)
+
+    return filename
+
+
+def convert_to_csv(filename: str):
+    """Convert dataset file to CSV for faster load in Pandas"""
+    csv_filename = str(Path(filename).with_suffix(".csv"))
+    if not (data_path / csv_filename).exists():
+        temp_file = tmp_path / csv_filename
+        Xlsx2csv(data_path / filename, dateformat="%Y-%m-%dT%H:%M:%S").convert(
+            str(temp_file)
+        )
+        shutil.move(temp_file, data_path / csv_filename)
+
+    return csv_filename
+
+
+class FilteringCsvReader:
+    """CSV file reader with a condition function to load only applicable rows"""
+
+    def __init__(self, filename: str, condition: Callable[[dict], bool]) -> None:
+        self.file = open(filename, "rt")
+        self.condition = condition
+        self.iterator = self.__iter__()
+
+    def read(self, count=-1):
+        return next(self.iterator, "")
+
+    def __iter__(self):
+        line = next(self.file)
+        headers = next(csv.reader([line]))
+        yield line
+
+        try:
+            while (line := next(self.file)) != None:
+                data = dict(zip(headers, next(csv.reader([line]))))
+                while not self.condition(data):
+                    line = next(self.file)
+                    data = dict(zip(headers, next(csv.reader([line]))))
+                yield line
+        except StopIteration:
+            return ""
+
+
+@st.cache_data
+def load_dataset(dataset, start_year, end_year):
+    dataset_file = download_dataset(dataset)
+    csv_file = convert_to_csv(dataset_file)
+    return pd.read_csv(
+        FilteringCsvReader(
+            data_path / csv_file,
+            lambda d: start_year <= int(d["YEAR"]) <= end_year,
+        )
+    )
+
+
+def main():
+    st.set_page_config(
+        page_title="ACLED",
+        page_icon="⚔️",
+    )
+
+    st.title("ACLED")
+
+    datasets = load_datasets()
+
+    start_year, end_year = st.select_slider(
+        "Date range",
+        options=range(2000, date.today().year + 1),
+        value=(date.today().year - 1, date.today().year),
+    )
+
+    selected_datasets = st.multiselect(
+        "Datasets", datasets, format_func=lambda d: d["name"]
+    )
+
+    if selected_datasets:
+
+        df = pd.concat(
+            map(
+                lambda d: load_dataset(d, start_year, end_year),
+                selected_datasets,
+            ),
+            keys=[d["name"] for d in selected_datasets],
+        )
+
+        item_count = st.slider("Items", 10, len(df), value=100)
+
+        st.header("Recent")
+        st.dataframe(df.sort_values("EVENT_DATE", ascending=False).head(item_count))
+
+        st.header("Most fatal")
+        st.dataframe(df.sort_values("FATALITIES", ascending=False).head(item_count))
+
+        fatalities_by_date_and_country = (
+            df.groupby(["EVENT_DATE", "COUNTRY"])["FATALITIES"]
+            .sum()
+            .reset_index()
+            .pivot(index="EVENT_DATE", columns="COUNTRY")
+            .FATALITIES.fillna(0)
+        )
+
+        st.header("Fatalities by date and country")
+        st.plotly_chart(px.line(fatalities_by_date_and_country))
+
+        st.header("Cumulative fatalities by date and country")
+        st.plotly_chart(px.line(fatalities_by_date_and_country.cumsum()))
 
 
 if __name__ == "__main__":
